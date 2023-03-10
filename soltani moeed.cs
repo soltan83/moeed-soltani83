20a83e7315d059d06e5afe885f5a8a9f164f4c77
soltan83 <127205469+soltan83@users.noreply.github.com>
2023-03-09 23:26:47 -0800

    Add files via upload

---
+++ soltani moeed.cs
@@ -0,0 +1,18 @@
+﻿using Sestem;
+public class program
+{
+    Console.Writeline(Is your father alive?)
+ Console.WriteLine("yes");      
+        cont= true
+        break;
+        console.Writeline(Is your mother alive?)
+ console.Witeline("yes");
+        cont= true
+        break;
+        while (cont);
+        }
+}
+}
+
+
+
